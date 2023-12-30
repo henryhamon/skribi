f1a112dcbf98beabeb3a379cbc96a266b5c921e9
Henry Hamon <henry@hamonrails.com.br>
2023-12-30 15:29:38 -0300

    🐛 demo hotfix

--- index.js
+++
@@ -1,5 +0,0 @@
-const Skribi = require('./skribi'); // Replace with the actual path to your Skribi class
-
-document.querySelectorAll("[data-skribi]").forEach( el => {
-    let s = new Skribi(el);
-});
